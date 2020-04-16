da8885b08be4edde176189c84799f58268e0400e
Alex Vinter <alex.vinter24@gmail.com>
2020-04-16 12:03:13 +0300

    project creation

---
+++ CarsStore/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsStore
+{
+    public class Cars
+    {
+        public int CarId { get; set; }
+        public int CarID { get; set; }
+        public int DriveTypeID { get; set; }
+        public int EngineDescriptionID { get; set; }
+        public int MakeID { get; set; }
+        public int ModelID { get; set; }
+        public int ConstructionYearID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int VehiclePrice { get; set; } 
+
+    }
+}
