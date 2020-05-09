853cacd3410c1e7fe40bbbc64dbbffd148bd4264
Alex Vinter <alex.vinter24@gmail.com>
2020-05-09 21:38:08 +0300

    no message

--- CarStoreApplication/Controllers/DBVehicleController.cs
+++ CarStoreApplication/Controllers/DBVehicleController.cs
@@ -13,21 +13,30 @@ namespace CarStoreApplication.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class DbContextVehicleController : ControllerBase
+    public class DBVehicleController : ControllerBase
     {
         private readonly VehiclesDBContext _context;
 
-        public DbContextVehicleController (VehiclesDBContext context)
+        public DBVehicleController (VehiclesDBContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// To get all the Vehicles from DB
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult GetVehicles()
         {
             return Ok(_context.VehiclesDbSet.ToList());
         }
 
+        /// <summary>
+        /// To get the Vehicles based on the vehicleID parameter
+        /// </summary>
+        /// <param name="vehicleID"></param>
+        /// <returns></returns>
         [HttpGet("{vehicleID}")]
         public IActionResult GetVehicleByID(int vehicleID)
         {
