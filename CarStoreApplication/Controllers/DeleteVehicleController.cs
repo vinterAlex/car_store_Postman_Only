e483a787fc67d90d8910e11c607e91164e495388
Alex Vinter <alex.vinter24@gmail.com>
2020-05-25 16:02:13 +0300

    Added Delete Controller
    DELETE http://localhost:51680/api/shop/deletevehicle?vehicleID=668

---
+++ CarStoreApplication/Controllers/DeleteVehicleController.cs
@@ -0,0 +1,52 @@
+﻿using VehicleUtils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CarStoreApplication.Controllers
+{
+    [ApiController]
+    [Route("api/shop/[controller]")]
+    public class DeleteVehicleController : ControllerBase
+    {
+        [HttpDelete]
+        public IActionResult DeleteVehicle(int vehicleID)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Utils.connectionString);
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = conn;
+                
+                cmd.CommandText = " delete from Vehicles " +
+                                  " where VehicleID = @vehicleID";
+
+                cmd.Parameters.AddWithValue("@vehicleID", vehicleID); // vehicleID for URI
+
+                conn.Open();
+
+                DataTable dt = new DataTable();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    dt.Load(dr);
+
+                    VehicleForCreation add_vehicle = new VehicleForCreation();
+
+                    return Ok("Vehicle "+vehicleID.ToString() +" Deleted successfully!");
+
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, sqlEx);
+            }
+        }
+    }
+}
